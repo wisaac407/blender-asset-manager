a45ce8c114a05643c1fade60c61ec88abd0c766c
Sybren A. Stüvel <sybren@stuvel.eu>
2017-01-17 16:04:13 +0100

    Removed superfluous forwarding of sys.argv

--- bam/__init__.py
+++ bam/__init__.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import sys
 __version__ = "1.0"
 
-def main(argv=sys.argv):
-    from .cli import main
-    sys.exit(main(argv[1:]))
-
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    from .cli import main
+
+    main()
